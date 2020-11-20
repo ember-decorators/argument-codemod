bbc02ae460cdbe20f943898a5c7e1ae6b006503a
Joshua Lawrence <jlawrence@linkedin.com>
2020-11-20 15:27:46 -0800

    Fixed hbs transform

--- transforms/argument-decorators-json-to-template/index.js
+++ transforms/argument-decorators-json-to-template/index.js
@@ -1,89 +1,90 @@
 const fs = require('fs');
 
-module.exports = function (env) {
-  const { filePath, syntax } = env;
-  const b = syntax.builders;
-  const filename = filePath.substring(filePath.lastIndexOf('/') + 1).slice(0, -4);
+module.exports = function ({ path, source }, { parse, visit }) {
+  const ast = parse(source);
+  const filename = path.substring(path.lastIndexOf('/') + 1).slice(0, -4);
   const argumentsJSON = fs.readFileSync('argument-decorators-to-json.json', {
     encoding: 'utf8',
   });
-
   const argumentsData = JSON.parse(argumentsJSON);
   const data = argumentsData[filename] ? argumentsData[filename].argumentProperties : {};
 
-  // Build out the positional arguments for the arg-type helper
-  const parseArgTypes = function parseArgTypes({ type, value, args, hash } = {}) {
-    const templateArgs = [];
+  return visit(ast, (env) => {
+    let { builders: b } = env.syntax;
 
-    if (type === 'Identifier') {
-      switch (value) {
-        case 'Function':
-        case 'ClosureAction':
-          templateArgs.push(b.string('function'));
-          break;
-        case 'Promise':
-          templateArgs.push(b.string('object'));
-          break;
-        default:
-          templateArgs.push(b.string('__UNKNOWN_TYPE__'));
+    // Build out the positional arguments for the arg-type helper
+    const parseArgTypes = function parseArgTypes({ type, value, args } = {}) {
+      const templateArgs = [];
+
+      if (type === 'Identifier') {
+        switch (value) {
+          case 'Function':
+          case 'ClosureAction':
+            templateArgs.push(b.string('function'));
+            break;
+          case 'Promise':
+            templateArgs.push(b.string('object'));
+            break;
+          default:
+            templateArgs.push(b.string('__UNKNOWN_TYPE__'));
+        }
       }
-    }
 
-    if (type === 'StringLiteral') {
-      templateArgs.push(b.string(value));
-    }
-
-    if (type === 'CallExpression') {
-      if (value === 'shapeOf') {
-        const shape = args[0].value;
-        const pairs = Object.entries(shape).map(([key, val]) => {
-          return b.pair(key, parseArgTypes(val)[0]);
-        });
-
-        templateArgs.push(b.sexpr(b.path(value), [], b.hash(pairs)));
-      } else {
-        // for each item in args, clall this func and set to posArgs
-        const posArgs = args
-          .map((arg) => {
-            return parseArgTypes(arg);
-          })
-          .flat();
-
-        templateArgs.push(b.sexpr(b.path(value), posArgs));
+      if (type === 'StringLiteral') {
+        templateArgs.push(b.string(value));
       }
-    }
 
-    return templateArgs;
-  };
+      if (type === 'CallExpression') {
+        if (value === 'shapeOf') {
+          const shape = args[0].value;
+          const pairs = Object.entries(shape).map(([key, val]) => {
+            return b.pair(key, parseArgTypes(val)[0]);
+          });
 
-  // Create the arg-type helper for a given argument property
-  const buildArgTypeHelper = function buildArgTypeHelper(key, val) {
-    const args = parseArgTypes(val);
-    return b.mustache(
-      b.path('arg-type'),
-      [b.path(`@${key}`), ...args],
-      b.hash([b.pair('path', b.string(key))])
-    );
-  };
+          templateArgs.push(b.sexpr(b.path(value), [], b.hash(pairs)));
+        } else {
+          // for each item in args, clall this func and set to posArgs
+          const posArgs = args
+            .map((arg) => {
+              return parseArgTypes(arg);
+            })
+            .flat();
 
-  // Get each arg-type helper, add a newline after it, and return the collection of all helpers
-  const argTypes = Object.entries(data).reduce((_hash, [key, val]) => {
-    _hash.push(buildArgTypeHelper(key, val));
-    _hash.push(b.path('\n'));
-    return _hash;
-  }, []);
-
-  // Update AST
-  return {
-    Template(node) {
-      // If there are any arg-types at the top of the file we can assume this has already been run
-      if (
-        !(node.body[0].type === 'MustacheStatement' && node.body[0].path.original === 'arg-type')
-      ) {
-        node.body = argTypes.concat([b.path('\n'), ...node.body]);
+          templateArgs.push(b.sexpr(b.path(value), posArgs));
+        }
       }
-    },
-  };
+
+      return templateArgs;
+    };
+
+    // Create the arg-type helper for a given argument property
+    const buildArgTypeHelper = function buildArgTypeHelper(key, val) {
+      const args = parseArgTypes(val);
+      return b.mustache(
+        b.path('arg-type'),
+        [b.path(`@${key}`), ...args],
+        b.hash([b.pair('path', b.string(key))])
+      );
+    };
+
+    // Get each arg-type helper, add a newline after it, and return the collection of all helpers
+    const argTypes = Object.entries(data).reduce((_hash, [key, val]) => {
+      _hash.push(buildArgTypeHelper(key, val));
+      _hash.push(b.path('\n'));
+      return _hash;
+    }, []);
+
+    return {
+      Template(node) {
+        // If there are any arg-types at the top of the file we can assume this has already been run
+        if (
+          !(node.body[0].type === 'MustacheStatement' && node.body[0].path.original === 'arg-type')
+        ) {
+          node.body = argTypes.concat([b.path('\n'), ...node.body]);
+        }
+      },
+    };
+  });
 };
 
 module.exports.type = 'hbs';
