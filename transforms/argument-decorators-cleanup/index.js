eadff6546a563bc125e9da96834e51bbeb54896d
Joshua Lawrence <jlawrence@linkedin.com>
2020-12-01 13:07:28 -0800

    Optionally add @classic decorator to components

--- transforms/argument-decorators-cleanup/index.js
+++ transforms/argument-decorators-cleanup/index.js
@@ -5,6 +5,55 @@ module.exports = function transformer(file, api) {
   const j = getParser(api);
   const options = getOptions(); // eslint-disable-line
   const input = j(file.source);
+
+  // Add classic decorators
+  if (options.addClassicDecorators) {
+    // Find ember classic decorator import
+    const classicDecoratorImportCollection = input.find(j.ImportDeclaration, {
+      source: {
+        value: 'ember-classic-decorator',
+      },
+    });
+    // Add ember classic decorator import
+    if (!classicDecoratorImportCollection.length) {
+      const importDeclarations = input.find(j.ImportDeclaration);
+      if (importDeclarations.length) {
+        // insert the import before other imports
+        j(input.find(j.ImportDeclaration).at(0).get()).insertBefore(
+          `import classic from 'ember-classic-decorator';`
+        );
+      } else {
+        // insert the import at top of the file
+        const file = input.get(j.File);
+        file.parentPath.value.program.body.unshift(
+          `import classic from 'ember-classic-decorator';`
+        );
+      }
+    }
+
+    // Add the @classic decorator
+    input
+      .find(j.ClassDeclaration, {
+        superClass: {
+          name: 'Component',
+        },
+      })
+      .forEach((path) => {
+        if (path.value.decorators) {
+          // Check if the @classic decorator already exists
+          const hasClassicDecorator = path.value.decorators.some(
+            (decorator) => decorator.expression.name === 'classic'
+          );
+
+          if (!hasClassicDecorator) {
+            path.value.decorators.unshift('@classic');
+          }
+        } else {
+          path.value.decorators = ['@classic'];
+        }
+      });
+  }
+
   // Remove decorators
   input.find(j.ClassBody).forEach((classBody) => {
     if (classBody.value.body && classBody.value.body.length) {
